43857fce11820e15c6e7364869e6d03b59be4778
soyeur.adrien <soyeur.adrien@gmail.com>
2022-04-28 21:31:47 +0200

    add og

--- head.php
+++ head.php
@@ -7,6 +7,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $appName ?><?= isset($title) ? " - $title" : '' ?></title>
+    <meta name="description" content="<?= $site_description ?>" />
+
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="<?= $appName ?><?= isset($title) ? " - $title" : '' ?>" />
+    <meta property="og:site_name" content="<?= $appName ?>" />
+    <?php if(isset($site_description)): ?>
+        <meta property="og:description" content="<?= $site_description ?>" />
+    <?php endif; ?>
+    <meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqgYX1R_WTACJuB8JPKVJJ0-MfKNEv9sW8ig&usqp=CAU" ?>
+    <meta property="article:author" content="<?=$appName ?>" />
+
+    <meta name="twitter:site" content="<?= $appName ?>" />
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:creator" content="<?= $appName ?>" />
+
+    <meta name="keywords" content="Bio, Restauration, Produits, Article" />
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
 </head>
